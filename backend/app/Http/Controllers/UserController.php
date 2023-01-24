9e116f5851cdf2e35671bde424a02c127ff49bbb
Matheus Felizardo <mfelizardo@envivent.com>
2023-01-24 01:01:43 +0000

    some methods to user and encrypting password

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Crypt;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return new UserResource($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {   
+        $validatorData = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users,email',
+            'password' => 'required',
+        ]);
+
+        if ($validatorData->fails()) {
+            return response()->json([
+                'errors' => $validatorData->errors()
+            ], 422);
+        }
+
+        // Password encrypt before save in database
+        $parsed_user = [
+            ...$validatorData->getData(),
+            'password' => Crypt::encryptString($validatorData->getData()['password'])
+        ];
+
+        $user = User::create($parsed_user);
+        return new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {   
+        $user = User::find($user->id);
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
